d76b92debd0754eaa4b54d80f60567a781702baa
Nitin Gupta <nitingupta08@yahoo.co.in>
2017-06-09 07:57:50 +0000

    Added motd and changed apache default for node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,16 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #Install httpd game
+
+if node ['paltform_family'] == "rhel"
+	package = "httpd"
+elsif node ['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	#package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -13,3 +21,5 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
+#include_recipe 'apache::websites'
